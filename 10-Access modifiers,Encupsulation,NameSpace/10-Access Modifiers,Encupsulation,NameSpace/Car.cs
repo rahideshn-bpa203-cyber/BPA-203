578ac3890636cb73bdbaa6a11eaeb79052f3d33c
Nuriyeva Rahide <rahideshn-bpa203@code.edu.az>
2025-10-24 05:48:50 +0400

    task10

---
+++ 10-Access modifiers,Encupsulation,NameSpace/10-Access Modifiers,Encupsulation,NameSpace/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Access_Modifiers_Encupsulation_NameSpace
+{
+    internal class GET
+
+    {
+        private int _HorsePower;
+        public int HorsePower
+        {
+            get { return _HorsePower; }
+            set 
+            {
+                if (value < 100) ;
+                Console.WriteLine("Please set correct power");
+                return;
+            
+            
+            
+            }
+            
+
+        }
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ 10-Access modifiers,Encupsulation,NameSpace/10-Access Modifiers,Encupsulation,NameSpace/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _10_Access_Modifiers_Encupsulation_NameSpace
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+
+            GET get = new GET();
+            get.HorsePower = 200;
+            Console.WriteLine(get.HorsePower);
+        }
+
+    }
+    
+}
