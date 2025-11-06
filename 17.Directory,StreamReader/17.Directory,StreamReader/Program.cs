206a41023045dc5f69e28f99deb87c291c7427fb
Nuriyeva Rahide <rahideshn-bpa203@code.edu.az>
2025-11-06 13:54:37 +0400

    Task17

---
+++ 17.Directory,StreamReader/17.Directory,StreamReader/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _17.Directory_StreamReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
