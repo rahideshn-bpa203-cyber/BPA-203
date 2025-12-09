d40f63bc7589cd98689e8c7aac3bb8cdd7b6bf4c
Nuriyeva Rahide <rahideshn-bpa203@code.edu.az>
2025-12-09 13:00:45 +0400

    32

---
+++ 32.MVC Intro/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCIntro32.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var student = new JsonResult(
+            //new
+            //{
+            //    Id = 1,
+            //    Name = "Rahide",
+            //    Surname = "Aliyev"
+            //}
+            //);
+            return View();
+        }
+        public IActionResult Detail(int? id)
+        {
+            if (id is null || id < 1)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+            return View();
+
+        }
+        public IActionResult Error()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ 32.MVC Intro/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCIntro32.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
